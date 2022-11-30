b7225dd5027db40dd08a80e379cbe43ff33f09e2
Darragh Downey <darragh.downey90@gmail.com>
2022-11-30 15:39:47 +1100

    1.5 repl done

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -3,33 +3,64 @@ package token
 type TokenType string
 
 type Token struct {
-  Type TokenType
-  Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers + literals
-  IDENT = "IDENT" // add, foobar, x, y, etc.
-  INT = "INT" // 132456
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, etc.
+	INT   = "INT"   // 132456
 
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-  //Delimiters
-  COMMA = ","
-  SEMICOLON = ";"
+	LT = "<"
+	GT = ">"
 
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
+	EQ     = "=="
+	NOT_EQ = "!="
 
-  // Keywords
-  FUNCTION = "FUNCTION"
-  LET = "LET"
+	//Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok // one of our languages keywords
+	}
+	return IDENT // user defined identifiers
+}
--- src/main.go
+++ src/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
 
-	"github.com/darragh-downey/twerp/pkg/token"
+	"github.com/darragh-downey/twerp/pkg/repl"
 )
 
 func main() {
-	fmt.Println("hello ", token.ASSIGN)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the TWERP programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in some commands!!\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
