3e3ccc42881ed99cb3a17ea19e67698355cf6286
Darragh Downey <darragh.downey90@gmail.com>
2022-12-02 16:46:18 +1100

    4.0 working interpreter - extending functionality

--- pkg/object/environment.go
+++ pkg/object/environment.go
@@ -1,16 +1,28 @@
 package object
 
+// Environment keeps track of of variables and functions within its scope
 type Environment struct {
-	store map[string]Object
+	store map[string]Object // a mapping for variables and functions
+	outer *Environment      // a pointer to an enclosing environment (think function scope)
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{s}
+	return &Environment{store: s, outer: nil}
 }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
+// Get will attempt to locate the value for the given identifier in the current and surrounding/enclosing scope
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
